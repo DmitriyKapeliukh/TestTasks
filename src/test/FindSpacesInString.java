a695a5457e3208f68d74588b89a0433e18f333bb
Dmytro Kapeliukh <dmytro.kapeliukh@b2broker.com>
2023-11-28 21:36:09 +0200

    Refactor structure

--- src/test/FindSpacesInString.java
+++ src/test/FindSpacesInString.java
@@ -19,4 +19,16 @@ public class FindSpacesInString {
         }
         return count;
     }
+
+    public static int containsWithCharacter(final String value){
+        int count = 0;
+        if(value != null){
+            for(int i = 0; i < value.length(); i++){
+                if(Character.isWhitespace(value.charAt(i))){
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
 }
